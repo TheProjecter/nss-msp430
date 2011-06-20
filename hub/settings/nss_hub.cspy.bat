2f490579f2dadc02ab2b5dde2be8e334c386d70c
ryanr <ryanr@nodleks.net>
2011-06-20 08:49:27 +0000

    Added voltage measurement methods for node.

--- hub/settings/nss_hub.cspy.bat
+++ hub/settings/nss_hub.cspy.bat
@@ -22,7 +22,7 @@
 @REM but they are listed at the end of this file for reference.
 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\common\bin\cspybat" "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\bin\430proc.dll" "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\bin\430fet.dll"  %1 --plugin "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\bin\<libsupport_plugin>" --backend -B "-p" "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\config\MSP430F2274.ddf" "--iv_base" "0xFFE0" "-d" "fet" "--erase_main_and_info" "--derivative" "MSP430F2274" "--protocol" "spy-bi-wire" "--eem" "EMEX_LOW" "--port" "Automatic" "--connection" "ti_usb" "--settlingtime=0" "--msp430_dll" "msp430.dll" 
+"C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\common\bin\cspybat" "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\bin\430proc.dll" "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\bin\430fet.dll"  %1 --plugin "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\bin\<libsupport_plugin>" --backend -B "-p" "C:\Program Files\IAR Systems\Embedded Workbench 6.0 Kickstart\430\config\MSP430F2274.ddf" "--iv_base" "0xFFE0" "-d" "fet" "--erase_main_and_info" "--derivative" "MSP430F2274" "--vccvoltage=3.0" "--protocol" "spy-bi-wire" "--eem" "EMEX_LOW" "--port" "Automatic" "--connection" "ti_usb" "--settlingtime=0" "--msp430_dll" "msp430.dll" 
 
 
 @REM Loaded plugins:
